ddcdd2cfdfb62a4500fcf815e22952d1f2e40a94
Real-Shoy <14712022100947@stud.cu.edu.eg>
2025-05-11 20:01:25 +0300

    Update Arduino.ino

--- X_O_game/Arduino.ino
+++ X_O_game/Arduino.ino
@@ -58,11 +58,11 @@ void HorizontalMove(int to = 0) {
 
 int nextCubepos = 0;
 int CubesPlaces[5][3] = {
-    {82, 40, 10},
-    {95, 45, 20}, 
-    {107, 50, 25}, 
-    {90, 20, 20},
-    {100, 30, 20}
+    {82, 40 - 12, 10},
+    {95, 45 - 12, 20}, 
+    {107, 50 - 12, 25}, 
+    {90, 20 - 12, 20},
+    {100, 30 - 12, 20}
     // big - rotational - baby
 };
 
@@ -133,47 +133,47 @@ void loop() {
     
     if (command == "top-left") {
         GetNextCube();
-        PutInPosition(107,77,35);
+        PutInPosition(107,77 - 12,35);
         Serial.println("done");
     }
     else if (command == "top-center") {
         GetNextCube();
-        PutInPosition(105,105,35);
+        PutInPosition(105,105 - 12,35);
         Serial.println("done");
     }
     else if (command == "top-right") {
         GetNextCube();
-        PutInPosition(105,125,35);
+        PutInPosition(105,125 - 12,35);
         Serial.println("done");
     }
     else if (command == "middle-left") {
         GetNextCube();
-        PutInPosition(80,75,35);
+        PutInPosition(80,75 - 12,35);
         Serial.println("done");
     }
     else if (command == "middle-center") {
         GetNextCube();
-        PutInPosition(75,100,35);
+        PutInPosition(75,100 - 12,35);
         Serial.println("done");
     }
     else if (command == "middle-right") {
         GetNextCube();
-        PutInPosition(78,130,35);
+        PutInPosition(78,130 - 12,35);
         Serial.println("done");
     }
     else if (command == "bottom-left") {
         GetNextCube();
-        PutInPosition(60,60,20);
+        PutInPosition(60,60 - 12,20);
         Serial.println("done");
     }
     else if (command == "bottom-center") {
         GetNextCube();
-        PutInPosition(55,100,20);
+        PutInPosition(55,100 - 12,20);
         Serial.println("done");
     }
     else if (command == "bottom-right") {
         GetNextCube();
-        PutInPosition(55,140,20);
+        PutInPosition(55,140 - 12,20);
         Serial.println("done");
     }
     else if (command == "Reset") {
